e23ca21317b331c172e5e4d7cf0ec666bbdae6e5
James Brownie <jamespbrownie@gmail.com>
2021-11-01 10:39:04 -0500

    commit tree d5ddc253ffc03d54c4a6a77f5c713ad5ce7ea4af

---
+++ app.rb
@@ -0,0 +1,21 @@
+# puts "Hello world!"
+# # puts "Hello world!"
+# # puts "Hello world!"
+
+# #this is a comment
+
+
+# # print "Hello world!"
+# # print "Hello world!"
+# # print "Hello world!"
+
+# p [1,2,3]
+# #p is same as print
+
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+# # pp stands for "pretty-print"
+
+
+puts "Hello World!"
+print "Pass this test, please."
+p [1, 2, 3]
